b4a5117a69c34e16c8c30d4833c8082254280b40
Aaftab <aftabsaifi2050@gmail.com>
2026-05-24 23:09:12 +0530

    Fixed BaseURL Double Slash to Single

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "https://task-manager-api-dkke.onrender.com//api",
+    baseURL: "https://task-manager-api-dkke.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
